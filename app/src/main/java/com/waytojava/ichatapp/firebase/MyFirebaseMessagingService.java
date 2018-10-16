528dcda81a4a603a11930e229a49f21ba11c5471
javatech147 <javatech147@gmail.com>
2018-10-16 10:10:40 +0530

    Initial commit

--- app/src/main/java/com/waytojava/ichatapp/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/waytojava/ichatapp/firebase/MyFirebaseMessagingService.java
@@ -18,4 +18,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
     }
+	
+	/*
+	// Get token in Any other places in the app
+                FirebaseInstanceId.getInstance().getInstanceId()
+                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.w(TAG, "getInstanceId failed", task.getException());
+                                    return;
+                                }
+
+                                // Get new Instance ID token
+                                String token = task.getResult().getToken();
+
+                                // Log and toast
+                                String msg = getString(R.string.msg_token_fmt, token);
+                                Log.d(TAG, msg);
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+	
+	*/
 }
